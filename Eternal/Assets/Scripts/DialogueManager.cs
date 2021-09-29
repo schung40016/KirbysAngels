11016408ce1d15792e0789bb1e036c32e69f18df
Jonathan Sum <777JonathanSum@gmail.com>
2021-09-29 19:36:44 -0300

    Adding the Multiple-Response Dialogue System
    Signed-off-by: Jonathan Sum <777JonathanSum@gmail.com>

---
+++ Eternal/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public NPC npc;
+
+    bool isTalking = false;
+
+    float distance;
+    float curResponseTracker = 0;
+
+    public GameObject player;
+    public GameObject dialogueUI;
+
+    public Text npcNameAtManager;
+    public Text npcDialogueBox;
+    public Text playerResponse;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if (distance <= 2.5f)
+        {
+
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                curResponseTracker++;
+                if (curResponseTracker >= npc.playerDialogue.Length - 1)
+                {
+                    curResponseTracker = npc.playerDialogue.Length - 1;
+                }
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                curResponseTracker--;
+                if (curResponseTracker < 0)
+                {
+                    curResponseTracker = 0;
+                }
+            }
+
+
+
+
+            // Click F key to talk after the mouse mouse cursor is on the NPC.
+            if (Input.GetKeyDown(KeyCode.F) && isTalking == false)
+            {
+                StartConversation();
+            }
+            else if (Input.GetKeyDown(KeyCode.F) && isTalking == true)
+            {
+                EndDialogue();
+            }
+
+
+            //Later we can factor out this large code block
+            if (curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[1];
+                }
+            }
+            else if (curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
+            {
+                playerResponse.text = npc.playerDialogue[1];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[2];
+                }
+            }
+            else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
+            {
+                playerResponse.text = npc.playerDialogue[2];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[3];
+                }
+            }
+        }
+    }
+
+
+    void StartConversation()
+    {
+        isTalking = true;
+        curResponseTracker = 0;
+        dialogueUI.SetActive(true);
+        npcNameAtManager.text = npc.npcName;
+        npcDialogueBox.text = npc.dialogue[0];
+
+
+    }
+
+    void EndDialogue()
+    {
+        isTalking = false;
+        dialogueUI.SetActive(false);
+    }
+}
+
