1299fa352d2d5c71ec32c6c3c8be402387c77080
schung40016 <55321253+schung40016@users.noreply.github.com>
2021-11-17 13:44:31 -0800

    Experience shop Update #1
    - Implemented a script called Experience shop which will handle all the data regarding the experience shop.
    - Changed the Pause menu so that it is players can open it by pressing esc.

---
+++ Eternal/Assets/Scripts/UI/ExperienceShop.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperienceShop : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private int healthTier = 1;
+    private int damageTier = 1;
+    private int comboTier = 1;
+    private int manaTier = 1;
+
+    [SerializeField] private int healthUpgradeCost = 300;
+    [SerializeField] private int damageUpgradeCost = 500;
+    [SerializeField] private int ComboUpgradeCost = 400;
+    [SerializeField] private int manaUpgradeCost = 300;
+
+    // Opens up the experience shop.
+    public void OpenExperienceShop()
+    {
+
+    }
+
+    // Allows player to buy and upgrade their max health.
+    public void BuyHealthUpgrade()
+    {
+        if ( player.playerExp >= healthTier * 300 && healthTier < 5)
+        {
+            healthTier += 1;
+            player.maxHealth += 100;
+            player.currentHealth = player.maxHealth;
+        }
+    }
+
+    // Allows player to buy and upgrade their max health.
+    public void BuyManaUpgrade()
+    {
+        if (player.playerExp >= manaTier * 300 && manaTier < 5)
+        {
+            manaTier += 1;
+            player.maxMana += 100;
+            player.currentMana = player.maxMana;
+        }
+    }
+
+    public int getHealthTier()
+    {
+        return healthTier;
+    }
+
+    public int getManaTier()
+    {
+        return manaTier;
+    }
+}
